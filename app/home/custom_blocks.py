952cf4679b391ed532aecfff855222c7474357a5
thibdx <ttibot@gmail.com>
2019-06-02 16:25:40 +0200

    working on models

--- app/home/custom_blocks.py
+++ app/home/custom_blocks.py
@@ -2,7 +2,7 @@ from wagtail.core import blocks
 
 class WdQueryBlock(blocks.StructBlock):
     query_intro = blocks.RichTextBlock(required=False)
-    query_sparql = blocks.TextBlock(help_text='Past here a Wikidata SPARQL request')
+    query_sparql = blocks.TextBlock(help_text='Past here a Wikidata SPARQL request. You can test it before at query.wikidata.org.')
 
     class Meta:
         icon = 'database'
--- app/home/models.py
+++ app/home/models.py
@@ -6,6 +6,7 @@ from taggit.models import TaggedItemBase
 
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
+from django.contrib.postgres.fields import ArrayField
 from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel, StreamFieldPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
@@ -14,16 +15,18 @@ from wagtail.core import blocks
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.documents.blocks import DocumentChooserBlock
 from wagtail.snippets.blocks import SnippetChooserBlock
+from wagtail.snippets.models import register_snippet
 from wagtail.embeds.blocks import EmbedBlock
 
 from home.custom_blocks import WdQueryBlock
 
-class HomePage(Page): # This class is not to be used and will be deleted by the end
+class HomePage(Page): # To be changed to EaHomePage
     pass
 
 class EaHomePage(Page):
+    # Making the intro editable from the admin panel
     intro = RichTextField(blank=True)
-
+    
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
     ]
@@ -35,14 +38,13 @@ class EaHomePage(Page):
         context['articlepages'] = articlepages
         return context
 
-class ArticlePageTag(TaggedItemBase):
-    content_object = ParentalKey(
-        'ArticlePage',
-        related_name='tagged_items',
-        on_delete=models.CASCADE
-    )
-
 class ArticlePage(Page):
+
+    '''
+    Articles pages handle both hand written articles and Wikidata query results.
+    It uses StremField for the best flexibility.
+    A StreamField block is generated for Wikidata queries results.
+    '''
 
     # Database fields
 
@@ -55,8 +57,8 @@ class ArticlePage(Page):
         ('quote', BlockQuoteBlock()),
         ('page', PageChooserBlock()),
         ('document', DocumentChooserBlock()),
-        ('snippet', SnippetChooserBlock()),
         ('embed', EmbedBlock()),
+        ('wikidata_query', WdQueryBlock()),
     ])
     
     date = models.DateField("Post date")
@@ -98,6 +100,17 @@ class ArticlePage(Page):
         ImageChooserPanel('feed_image'),
     ]
 
+class ArticleTagPage(TaggedItemBase):
+    '''
+    Add tagging capacity to pages.
+    Based on Django tag system.
+    '''
+    content_object = ParentalKey(
+        'ArticlePage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
 class ArticleTagIndexPage(Page):
 
     def get_context(self, request):
@@ -109,4 +122,48 @@ class ArticleTagIndexPage(Page):
         # Update template context
         context = super().get_context(request)
         context['articlepages'] = articlepages
-        return context
+        return context=
+
+class ItemPage(Page):
+    
+    '''
+    This pages get a Wikidata's item Qid as an URL parameter.
+    It display :
+    - The Item name as a printed title
+    - The item description as a subheading
+    - The item alias
+    - The intro from the related Wikipedia article if it exists, 
+        - with a "read more on wikipedia" link
+        - and links to other articles gathered from Wikidata's external Qids
+    - The list of Wikidata's properties + values + qualifiers
+        - This list has a "Display more" that hides long content
+        - It can be troncated for specific "Instance of" class Qids according to a list of Pids
+        - else, it displays the 10 first Pids.
+        - It also displays a link "edit on Wikidata"
+    - A list of scholarly articles gathered on Wikidata
+        - Each article dislays title + date + DOI
+        - This list has a "Display more" that hides long content
+        - Only the first 10 more recent articles are loaded by the SPARQL in order to avoid very long load times
+        - At the end on the list there is a link "see articles from" linking to scholarly articles databases prefilled search
+    - A tag cloud / graph of nearby items can be generated on demand (long load time)
+        - Links to other visual insights sites are provided
+        - Other visual tools & graph algortyhms could be further added
+    '''
+
+    pass
+
+
+@register_snippet
+class InstanceOfQidParams(models.Model):
+    instance_of_Qid = models.CharField(max_length=255)
+    featured_Pids = ArrayField(
+            models.CharField(max_length=255, blank=True)
+        )
+
+    panels = [
+        FieldPanel('instance_of_Qid'),
+        FieldPanel('featured_Pids'),
+    ]
+
+    def __str__(self):
+        return self.instance_of_Qid
